2221c59ff08748f61f8f0d2f7db1bfd7cecbc801
ashok <asub23@gmail.com>
2024-10-19 00:08:57 -0400

    This is first commit of db

---
+++ shoe_dbinit.php
@@ -0,0 +1,11 @@
+<?php
+// dbinit.php
+$servername = "localhost";
+$username = "root";  // Your DB username
+$password = "";      // Your DB password
+$dbname = "shoe_ecommerce";  // Your DB name
+//This is for the database to connect to the server
+
+}
+?>
+
