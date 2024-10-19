2b7d4f6db8c351f19dc0572d9d0f92dd73438240
ashok <asub23@gmail.com>
2024-10-19 00:16:55 -0400

    This is db initialization and 3nd commit after 2nd one showing error if error occurs

--- shoe_dbinit.php
+++ shoe_dbinit.php
@@ -5,7 +5,10 @@ $username = "root";  // Your DB username
 $password = "";      // Your DB password
 $dbname = "shoe_ecommerce";  // Your DB name
 //This is for the database to connect to the server
+$conn = new mysqli($servername, $username, $password, $dbname);
 
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
 ?>
 
